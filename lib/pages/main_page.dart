4ca4bc381ce05c11f9658233565d78d814080741
Lubette <jinhong.zeng@mail.ru>
2025-04-19 19:09:02 +0800

    这是一次非常糟糕更新
    我打算下次完全重构这个代码，是完全完全的重构，已经非常无语了对自己的代码，或许Aim会完全转向，希望未来会对我好一点，谢谢
    
    Co-Authored-By: Nlub <103884480+zjhzzy@users.noreply.github.com>

--- lib/pages/main_page.dart
+++ lib/pages/main_page.dart
@@ -5,6 +5,7 @@ import 'package:aim/controls/main_control.dart';
 import 'package:aim/data/group_entity.dart';
 import 'package:aim/data/todo_entity.dart';
 import 'package:aim/views/todo_view.dart';
+import 'package:easy_sidemenu/easy_sidemenu.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_expandable_fab/flutter_expandable_fab.dart';
 import 'package:get/get.dart';
@@ -14,7 +15,47 @@ class MainPage extends StatelessWidget {
   final selected = 0.obs;
 
   MainPage({super.key});
-
+  List items = [
+    SideMenuItem(
+      title: 'Dashboard',
+      onTap: (index, _) {
+        sideMenu.changePage(index);
+      },
+      icon: Icon(Icons.home),
+      badgeContent: Text(
+        '3',
+        style: TextStyle(color: Colors.white),
+      ),
+    ),
+    SideMenuExpansionItem(
+      title: "Expansion Item",
+      icon: const Icon(Icons.kitchen),
+      onTap: (index, _, isExpanded) => {
+        print('$index, expanded $isExpanded')
+      },
+      children: [
+        SideMenuItem(
+          title: 'Expansion Item 1',
+          onTap: (index, _) {
+            sideMenu.changePage(index);
+          },
+          icon: const Icon(Icons.home),
+        ),
+        SideMenuItem(
+          title: 'Expansion Item 2',
+          onTap: (index, _) {
+            sideMenu.changePage(index);
+          },
+          icon: const Icon(Icons.supervisor_account),
+        )
+      ],
+    ),
+    SideMenuItem(
+      title: 'Exit',
+      onTap: () {},
+      icon: Icon(Icons.exit_to_app),
+    ),
+  ];
   @override
   Widget build(BuildContext context) {
     if (Get.find<MainControl>().groups.isEmpty) {
